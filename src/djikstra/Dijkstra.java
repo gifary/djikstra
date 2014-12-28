71e01292d04f4179e2d61ce9509b52036cb7f193
gifary <gifary@gifary-rg>
2014-12-28 13:01:20 +0700

    commit

--- src/djikstra/Dijkstra.java
+++ src/djikstra/Dijkstra.java
@@ -1,151 +1,100 @@
 package djikstra;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.NavigableSet;
-import java.util.TreeSet;
  
 public class Dijkstra {
    private static final Graph.Edge[] GRAPH = {
-      new Graph.Edge("a", "b", 7),
-      new Graph.Edge("a", "c", 9),
-      new Graph.Edge("a", "f", 14),
-      new Graph.Edge("b", "c", 10),
-      new Graph.Edge("b", "d", 15),
-      new Graph.Edge("c", "d", 11),
-      new Graph.Edge("c", "f", 2),
-      new Graph.Edge("d", "e", 6),
-      new Graph.Edge("e", "f", 9),
+      new Graph.Edge("T1", "A", 2),
+      new Graph.Edge("T1", "B", 3),
+      new Graph.Edge("T1", "C", 4),
+      new Graph.Edge("T1", "D", 3),
+      new Graph.Edge("T1", "G", 4),
+      new Graph.Edge("T1", "J", 5),
+      new Graph.Edge("T1", "M", 6),
+      new Graph.Edge("T1", "P", 7),
+      new Graph.Edge("T1", "K1", 6),
+      new Graph.Edge("A", "K1", 6),
+      new Graph.Edge("D", "K1", 5),
+      new Graph.Edge("G", "K1", 4),
+      new Graph.Edge("J", "K1", 3),
+      new Graph.Edge("M", "K1", 2),
+      new Graph.Edge("P", "K1", 1),
+      new Graph.Edge("A","B",1),
+      new Graph.Edge("A","D",1),
+      new Graph.Edge("B","C",1),
+      new Graph.Edge("B","E",1),
+      new Graph.Edge("C","F",1),
+      new Graph.Edge("D","E",1),
+      new Graph.Edge("D","G",1),
+      new Graph.Edge("E","F",1),
+      new Graph.Edge("E","H",1),
+      new Graph.Edge("F","I",1),
+      new Graph.Edge("G","H",1),
+      new Graph.Edge("G","J",1),
+      new Graph.Edge("H","K",1),
+      new Graph.Edge("H","I",1),
+      new Graph.Edge("I","L",1),
+      new Graph.Edge("J","K",1),
+      new Graph.Edge("J","M",1),
+      new Graph.Edge("K","L",1),
+      new Graph.Edge("K","N",1),
+      new Graph.Edge("L","O",1),
+      new Graph.Edge("M","N",1),
+      new Graph.Edge("M","P",1),
+      new Graph.Edge("N","Q",1),
+      new Graph.Edge("N","O",1),
+      new Graph.Edge("O","R",1),
+      new Graph.Edge("P","Q",1),
+      new Graph.Edge("Q","R",1),
+      //rute sebaliknya
+      new Graph.Edge("B","A",1),
+      new Graph.Edge("D","A",1),
+      new Graph.Edge("C","B",1),
+      new Graph.Edge("E","B",1),
+      new Graph.Edge("F","C",1),
+      new Graph.Edge("E","D",1),
+      new Graph.Edge("G","D",1),
+      new Graph.Edge("F","E",1),
+      new Graph.Edge("H","E",1),
+      new Graph.Edge("I","F",1),
+      new Graph.Edge("H","J",1),
+      new Graph.Edge("J","G",1),
+      new Graph.Edge("K","H",1),
+      new Graph.Edge("I","H",1),
+      new Graph.Edge("L","I",1),
+      new Graph.Edge("K","J",1),
+      new Graph.Edge("M","J",1),
+      new Graph.Edge("L","K",1),
+      new Graph.Edge("N","K",1),
+      new Graph.Edge("O","L",1),
+      new Graph.Edge("N","M",1),
+      new Graph.Edge("P","M",1),
+      new Graph.Edge("Q","N",1),
+      new Graph.Edge("O","N",1),
+      new Graph.Edge("R","O",1),
+      new Graph.Edge("Q","P",1),
+      new Graph.Edge("R","Q",1),
+      
    };
-   private static final String START = "b";
-   private static final String END = "f";
+  
+   
+   private static final String START ="A";
+   private static final String END = "M";
  
    public static void main(String[] args) {
-      Graph g = new Graph(GRAPH);
-      Graph g2 = new Graph(GRAPH);
-      g.dijkstra(START);
+     
+      
+	  Graph g = new Graph(GRAPH);
+    //  Graph g2 = new Graph(GRAPH);
+      //Edge edge[]=new Edge[10];
+    
+     g.dijkstra(START);
       g.printPath(END);
-      g2.dijkstra("c");
-      g2.printPath("f");
+      /* 
+      g2.dijkstra("H");
+      g2.printPath("K1");
+      */
       //g.printAllPaths();
    }
 }
  
-class Graph {
-   private final Map<String, Vertex> graph; // mapping of vertex names to Vertex objects, built from a set of Edges
-   
-   /** One edge of the graph (only used by Graph constructor) */
-   public static class Edge {
-      public final String v1, v2;
-      public final int dist;
-      public Edge(String v1, String v2, int dist) {
-         this.v1 = v1;
-         this.v2 = v2;
-         this.dist = dist;
-      }
-   }
- 
-   /** One vertex of the graph, complete with mappings to neighbouring vertices */
-   public static class Vertex implements Comparable<Vertex> {
-      public final String name;
-      public int dist = Integer.MAX_VALUE; // MAX_VALUE assumed to be infinity
-      public Vertex previous = null;
-      public final Map<Vertex, Integer> neighbours = new HashMap<>();
- 
-      public Vertex(String name) {
-         this.name = name;
-      }
- 
-      private void printPath() {
-         if (this == this.previous) {
-            System.out.printf("%s", this.name);
-         } else if (this.previous == null) {
-            System.out.printf("%s(unreached)", this.name);
-         } else {
-            this.previous.printPath();
-            System.out.printf(" -> %s(%d)", this.name, this.dist);
-         }
-      }
- 
-      public int compareTo(Vertex other) {
-         return Integer.compare(dist, other.dist);
-      }
-   }
- 
-   /** Builds a graph from a set of edges */
-   public Graph(Edge[] edges) {
-      graph = new HashMap<>(edges.length);
- 
-      //one pass to find all vertices
-      for (Edge e : edges) {
-         if (!graph.containsKey(e.v1)) graph.put(e.v1, new Vertex(e.v1));
-         if (!graph.containsKey(e.v2)) graph.put(e.v2, new Vertex(e.v2));
-      }
- 
-      //another pass to set neighbouring vertices
-      for (Edge e : edges) {
-         graph.get(e.v1).neighbours.put(graph.get(e.v2), e.dist);
-         //graph.get(e.v2).neighbours.put(graph.get(e.v1), e.dist); // also do this for an undirected graph
-      }
-   }
- 
-   /** Runs dijkstra using a specified source vertex */ 
-   public void dijkstra(String startName) {
-      if (!graph.containsKey(startName)) {
-         System.err.printf("Graph doesn't contain start vertex \"%s\"\n", startName);
-         return;
-      }
-      final Vertex source = graph.get(startName);
-      NavigableSet<Vertex> q = new TreeSet<>();
- 
-      // set-up vertices
-      for (Vertex v : graph.values()) {
-         v.previous = v == source ? source : null;
-         v.dist = v == source ? 0 : Integer.MAX_VALUE;
-         q.add(v);
-      }
- 
-      dijkstra(q);
-   }
- 
-   /** Implementation of dijkstra's algorithm using a binary heap. */
-   private void dijkstra(final NavigableSet<Vertex> q) {      
-      Vertex u, v;
-      while (!q.isEmpty()) {
- 
-         u = q.pollFirst(); // vertex with shortest distance (first iteration will return source)
-         if (u.dist == Integer.MAX_VALUE) break; // we can ignore u (and any other remaining vertices) since they are unreachable
- 
-         //look at distances to each neighbour
-         for (Map.Entry<Vertex, Integer> a : u.neighbours.entrySet()) {
-            v = a.getKey(); //the neighbour in this iteration
- 
-            final int alternateDist = u.dist + a.getValue();
-            if (alternateDist < v.dist) { // shorter path to neighbour found
-               q.remove(v);
-               v.dist = alternateDist;
-               v.previous = u;
-               q.add(v);
-            } 
-         }
-      }
-   }
- 
-   /** Prints a path from the source to the specified vertex */
-   public void printPath(String endName) {
-      if (!graph.containsKey(endName)) {
-         System.err.printf("Graph doesn't contain end vertex \"%s\"\n", endName);
-         return;
-      }
- 
-      graph.get(endName).printPath();
-      System.out.println();
-   }
-   /** Prints the path from the source to every vertex (output order is not guaranteed) */
-   public void printAllPaths() {
-      for (Vertex v : graph.values()) {
-         v.printPath();
-         System.out.println();
-      }
-   }
-}
+
